1276386e349843670ccc38a4dca87b497bcb8355
jxy <656385934@qq.com>
2022-03-13 00:21:01 +0800

    refactor: use GNU time to monitor execution

--- mail-run.py
+++ mail-run.py
@@ -5,8 +5,11 @@ from email.header import Header
 import sys
 import subprocess
 import time
+import os
+from uuid import uuid4
 from datetime import timedelta
 from functools import reduce
+
 
 MAIL_HOST = "smtp.qq.com"
 SMTP_PORT = 587
@@ -15,71 +18,43 @@ MAIL_FROM = "mail-run notifier <%s>" % MAIL_SENDER
 MAIL_LICENCE = "123456"
 MAIL_RECEIVERS = ["somebody@gmail.com", "Jack@qq.com"]
 
-cmd_str = reduce(lambda x, y: x + " " + y, sys.argv[1:])
-target_program = cmd_str.split()[0]
-start_time = time.time()
-ret = subprocess.run(cmd_str, shell=True)
 
-# Setting subject.
-if ret.returncode == 0:
-    subject = "process [%s] " % target_program + "ends normally."
-else:
-    subject = "process [%s] " % target_program + "ends with an error, return code: %d" % ret.returncode
-
-# Setting running time.
-end_time = time.time()
-running_time = end_time - start_time
-
-html_msg = """<!DOCTYPE html>
-<html>
-<head>
-<style>
-* {
-          font-family: "Lucida Console", "Courier New", monospace;
-          }
+def stripped_time_record_list(time_record_file_path) -> list:
+    stripped = []
+    with open(time_record_file_path) as trf:
+        for line in trf:
+            stripped.append(line.strip())
+    return stripped
 
 
-.thick {
-          font-weight: bold;
-          }
+def make_mail_body(time_record_file_path):
+    return "\n".join(stripped_time_record_list(time_record_file_path))
 
-</style>
-</head>
-<body>
-<table>
-<tr>
-  <td><span class="thick">command:</span></td>""" +\
-"  <td>\"%s\"\n</td>" % " ".join(sys.argv[1:]) +\
-"""</tr>
-<tr>
-  <td><span class="thick">starts at:</span></td>""" +\
-"  <td>%s\n</td>" % time.ctime(start_time) +\
-"""</tr>
-<tr>
-  <td><span class="thick">ends at:</span></td>""" +\
-"  <td>%s\n</td>" % time.ctime(end_time) +\
-"""</tr>
-<tr>
-  <td><span class="thick">running time:</span></td>""" +\
-"  <td>%f sec.\n</td>" % running_time  +\
-"""</tr>
-<tr>
-  <td><span class="thick">timedelta:</span></td>""" +\
-"  <td>%s</td>" % timedelta(seconds=running_time) +\
-"""</tr>
-</table>
-</body>
-</html>"""
 
-message = MIMEText(html_msg, "html", "utf-8")
-message['From'] = MAIL_FROM
-message['Subject'] = Header(subject, "utf-8")
+def main():
+    target_cmd = reduce(lambda x, y: x + " " + y, sys.argv[1:])
+    time_record_file_path = "/tmp/time-out-" + str(uuid4()) + ".tmp"
+    execution_cmd = " ".join(["time -v -o ", time_record_file_path, target_cmd])
+    completed = subprocess.run(execution_cmd, shell=True)
 
-try:
-    smtp = smtplib.SMTP()
-    smtp.connect(MAIL_HOST, SMTP_PORT)
-    smtp.login(MAIL_SENDER, MAIL_LICENCE)
-    smtp.sendmail(MAIL_SENDER, MAIL_RECEIVERS, message.as_string())
-    print("mail successfully sent.")
-except smtplib.SMTPException as e:
-    print("unable to send mails.", e)
+    exit_status = completed.returncode
+    subject = "\"%s\"" % target_cmd + " ends with status %s." % exit_status
+
+    message = MIMEText(make_mail_body(time_record_file_path), "plain", "utf-8")
+    message['From'] = MAIL_FROM
+    message['Subject'] = Header(subject, "utf-8")
+
+    try:
+        smtp = smtplib.SMTP()
+        smtp.connect(MAIL_HOST, SMTP_PORT)
+        smtp.login(MAIL_SENDER, MAIL_LICENCE)
+        smtp.sendmail(MAIL_SENDER, MAIL_RECEIVERS, message.as_string())
+        print("mail successfully sent.")
+    except smtplib.SMTPException as e:
+        print("unable to send mails.", e)
+    finally:
+        os.remove(time_record_file_path)
+
+
+if __name__ == "__main__":
+    main()
